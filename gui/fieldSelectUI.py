c45fef1cc8b59d4a0975a8a257dd614971d067fe
Josef Brandt <josef.brandt@gu.se>
2021-11-10 08:37:40 +0100

    Make FieldSelector work with very long code lists
    (only show the first 500 entries, use refining to explore more)

--- gui/fieldSelectUI.py
+++ gui/fieldSelectUI.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtWidgets, QtCore
+from PyQt6 import QtWidgets, QtCore, QtGui
 from typing import *
 
 from dataimport.domeCodes import DomeCode
@@ -35,35 +35,37 @@ class CodeSelector(QtWidgets.QDialog):
     """
     CodeSelected: QtCore.pyqtSignal = QtCore.pyqtSignal(DomeCode)  # Emitted with the code when clicking an entry
 
-    def __init__(self, codes: List['DomeCode']) -> None:
+    def __init__(self, codes: List['DomeCode'], maxNumToShow: int = 500) -> None:
         super(CodeSelector, self).__init__()
         self.setWindowTitle("Select Entry")
         self.setModal(True)
 
+        self._codes: List['DomeCode'] = codes
+        self._maxNumToShow: int = maxNumToShow
+        self._btns: List[QtWidgets.QPushButton] = []
+
         self._searchBar: QtWidgets.QLineEdit = QtWidgets.QLineEdit()
         self._searchBar.setPlaceholderText("Type for refining selection.")
-        self._searchBar.textEdited.connect(self._refineOptions)
+        self._searchBar.textEdited.connect(self._displayBtns)
         layout: QtWidgets.QVBoxLayout = QtWidgets.QVBoxLayout()
         layout.addWidget(QtWidgets.QLabel("Please select an entry from the list.\n"
                                           "Hover mouse over description for more info."))
+
+        if len(codes) > self._maxNumToShow:
+            boldFont: QtGui.QFont = QtGui.QFont()
+            boldFont.setBold(True)
+            warnLbl: QtWidgets.QLabel = QtWidgets.QLabel(f"Warning, large number of items ({len(codes)}), "
+                                                         f"only showing the first {self._maxNumToShow} hits. Please use searchbar for refining.")
+            warnLbl.setFont(boldFont)
+            layout.addWidget(warnLbl)
+
         layout.addWidget(self._searchBar)
 
         btnGroup: QtWidgets.QGroupBox = QtWidgets.QGroupBox()
         btnGroup.setFlat(True)
-        btnLayout: QtWidgets.QFormLayout = QtWidgets.QFormLayout()
-        btnLayout.addRow(QtWidgets.QLabel("Code"), QtWidgets.QLabel("Description"))
-        btnGroup.setLayout(btnLayout)
-
-        self._btns: List[QtWidgets.QPushButton] = []
-        self._lables: List[QtWidgets.QLabel] = []
-        for code in codes:
-            newBtn: QtWidgets.QPushButton = self._getPushButton(code)
-            newLbl: QtWidgets.QLabel = QtWidgets.QLabel(code.descr)
-            newLbl.setToolTip(code.long_descr)
-
-            self._btns.append(newBtn)
-            self._lables.append(newLbl)
-            btnLayout.addRow(newBtn, newLbl)
+        self._btnLayout: QtWidgets.QFormLayout = QtWidgets.QFormLayout()
+        btnGroup.setLayout(self._btnLayout)
+        self._displayBtns("")  # i.e., display all buttons.
 
         scrollArea: QtWidgets.QScrollArea = QtWidgets.QScrollArea()
         scrollArea.setWidget(btnGroup)
@@ -72,13 +74,34 @@ class CodeSelector(QtWidgets.QDialog):
         self.setLayout(layout)
 
     @QtCore.pyqtSlot(str)
-    def _refineOptions(self, searchText: str) -> None:
-        searchText = searchText.lower()
-        showAll: bool = searchText == ""
-        for btn, lbl in zip(self._btns, self._lables):
-            textMatch: bool = lbl.text().lower().find(searchText) != -1
-            btn.setVisible(showAll or textMatch)
-            lbl.setVisible(showAll or textMatch)
+    def _displayBtns(self, pattern: str, minPatternLength: int = 3) -> None:
+        """
+        Creates and displays button for the codes of this instance. Only codes that have a description containing the
+        given filter pattern are shown.
+        All codes are displayed if the pattern is shorter
+        :param pattern: The string to search for
+        :param minPatternLength: Minimum length of the pattern. If the pattern is shorter, all entries are shown
+        :return:
+        """
+        showAll: bool = len(pattern) < minPatternLength
+        if len(pattern) >= minPatternLength or showAll:
+            self._clearBtnLayout()
+            self._btns = []
+            pattern = pattern.lower()
+            self._btnLayout.addRow(QtWidgets.QLabel("Code"), QtWidgets.QLabel("Description"))
+            for code in self._codes:
+                if len(self._btns) < self._maxNumToShow:
+                    if code.descr.lower().find(pattern) != -1 or showAll:
+                        newBtn: QtWidgets.QPushButton = self._getPushButton(code)
+                        newLbl: QtWidgets.QLabel = QtWidgets.QLabel(code.descr)
+                        newLbl.setToolTip(code.long_descr)
+                        self._btnLayout.addRow(newBtn, newLbl)
+                        self._btns.append(newBtn)
+
+    def _clearBtnLayout(self) -> None:
+        for i in reversed(range(self._btnLayout.count())):
+            item = self._btnLayout.itemAt(i)
+            self._btnLayout.removeWidget(item.widget())
 
     def _getPushButton(self, code: 'DomeCode') -> QtWidgets.QPushButton:
         newBtn: QtWidgets.QPushButton = QtWidgets.QPushButton(code.code)
